a8fe8224bf6c602708281c7e0b4ce64edbaaa7d9
Tarrence van As <tarrencev@users.noreply.github.com>
2020-09-22 18:31:53 -0400

    chore(webm): opus test (#66)

--- pkg/elements/webm_test.go
+++ pkg/elements/webm_test.go
@@ -2,6 +2,7 @@ package elements
 
 import (
 	"bytes"
+	"sync"
 	"testing"
 
 	"github.com/at-wat/ebml-go"
@@ -12,6 +13,7 @@ import (
 
 // BufWriter instance
 type BufWriter struct {
+	sync.Mutex
 	buf bytes.Buffer
 }
 
@@ -23,6 +25,8 @@ func (w *BufWriter) ID() string {
 	return ""
 }
 func (w *BufWriter) Write(sample *avp.Sample) error {
+	w.Lock()
+	defer w.Unlock()
 	_, err := w.buf.Write(sample.Payload.([]byte))
 	return err
 }
@@ -39,6 +43,14 @@ type Header struct {
 	Segment webm.Segment    `ebml:"Segment,size=unknown"`
 }
 
+// VP8 keyframe packet
+var rawKeyframePkt = []byte{
+	0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
+	0x27, 0x82, 0x00, 0x01, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0x98, 0x36, 0xbe, 0x88, 0x9e,
+}
+
+var rawOpusPkt = []byte{0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x90, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x90}
+
 func TestWebMSaver_BlockWriterInit(t *testing.T) {
 	saver := NewWebmSaver(WebmSaverConfig{
 		ID: "id",
@@ -48,23 +60,27 @@ func TestWebMSaver_BlockWriterInit(t *testing.T) {
 	err := saver.Attach(writer)
 	assert.NoError(t, err)
 
-	// Construct keyframe packet
-	rawKeyframePkt := []byte{
-		0x90, 0xe0, 0x69, 0x8f, 0xd9, 0xc2, 0x93, 0xda, 0x1c, 0x64,
-		0x27, 0x82, 0x00, 0x01, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0x98, 0x36, 0xbe, 0x88, 0x9e,
-	}
-
 	err = saver.Write(&avp.Sample{
 		Type:    avp.TypeVP8,
 		Payload: rawKeyframePkt,
 	})
 	assert.NoError(t, err)
 
+	err = saver.Write(&avp.Sample{
+		Type:    avp.TypeOpus,
+		Payload: rawOpusPkt,
+	})
+	assert.NoError(t, err)
+
 	var header Header
+	writer.Lock()
 	err = ebml.Unmarshal(bytes.NewReader(writer.buf.Bytes()), &header)
 	assert.NoError(t, err)
+	writer.Unlock()
 
 	assert.NotNil(t, header.Header)
 
 	saver.Close()
+
+	assert.Len(t, header.Segment.Tracks.TrackEntry, 2)
 }
