83820898b3c1e3d345e4c9e98981bbf6e6c2e23a
cgojin <gongjin21@hotmail.com>
2020-10-20 21:15:49 +0800

    fix(r): registry init

--- pkg/avp.go
+++ pkg/avp.go
@@ -4,7 +4,7 @@ var registry *Registry
 
 // Init avp with a registry of elements
 func Init(elems map[string]ElementFun) {
-	registry := NewRegistry()
+	registry = NewRegistry()
 	for eid, elem := range elems {
 		registry.AddElement(eid, elem)
 	}
