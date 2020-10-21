d6504ac8eec6bf5942984e9ac6b0a6de7c9f62fc
cgojin <gongjin21@hotmail.com>
2020-10-21 12:44:58 +0800

    fix(docker): add main.go template

--- cmd/signal/grpc/main.go
+++ cmd/signal/grpc/main.go
@@ -1,1 +1,75 @@
-package grpc
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	grpc "github.com/pion/ion-avp/cmd/signal/grpc/server"
+	avp "github.com/pion/ion-avp/pkg"
+	"github.com/spf13/viper"
+)
+
+var (
+	conf = avp.Config{}
+	file string
+	addr string
+)
+
+func showHelp() {
+	fmt.Printf("Usage:%s {params}\n", os.Args[0])
+	fmt.Println("      -c {config file}")
+	fmt.Println("      -a {listen addr}")
+	fmt.Println("      -h (show help info)")
+}
+
+func load() bool {
+	_, err := os.Stat(file)
+	if err != nil {
+		return false
+	}
+
+	viper.SetConfigFile(file)
+	viper.SetConfigType("toml")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("config file %s read failed. %v\n", file, err)
+		return false
+	}
+	err = viper.GetViper().Unmarshal(&conf)
+	if err != nil {
+		fmt.Printf("config file %s loaded failed. %v\n", file, err)
+		return false
+	}
+
+	fmt.Printf("config %s load ok!\n", file)
+	return true
+}
+
+func parse() bool {
+	flag.StringVar(&file, "c", "config.toml", "config file")
+	flag.StringVar(&addr, "a", ":50052", "address to use")
+	help := flag.Bool("h", false, "help info")
+	flag.Parse()
+	if !load() {
+		return false
+	}
+
+	if *help {
+		showHelp()
+		return false
+	}
+	return true
+}
+
+func main() {
+	if !parse() {
+		showHelp()
+		os.Exit(-1)
+	}
+
+	grpc.NewServer(addr, conf, map[string]avp.ElementFun{
+		// fill in elements
+	})
+}
